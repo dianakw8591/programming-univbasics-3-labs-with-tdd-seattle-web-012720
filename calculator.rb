3d0d4453fbd15514e960f150f9a2c0787198094c
Diana Wendt <dianakw@gmail.com>
2019-12-20 17:20:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,7 @@
 first_number = 1
 second_number = 2
 
-def sum(first_number, second_number)
-  first_number + second_number
-end
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number*second_number
+quotient = first_number / second_number
